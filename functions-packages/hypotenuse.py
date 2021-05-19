afa0b6b2ded82d6dc138b6efeddce0f5e79087d4
qadr98 <69320578+qadr98@users.noreply.github.com>
2021-05-19 07:31:45 +0600

    Update hypotenuse.py

--- functions-packages/hypotenuse.py
+++ functions-packages/hypotenuse.py
@@ -1,3 +1,4 @@
 import math   
-def get_hypotenuse(catet1, catet2):  
-    # calculate hypotenuse of right triangle based on 2 catets and return it
+def get_hypotenuse(catet1, catet2):
+    c = catet1**2 + catet2**2
+    return sqrt(c**0.5)
